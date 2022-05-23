9e2d4199b9b6f196fe4457410690278c7a33a8d0
kfconeone <kfconeone@gmail.com>
2022-05-23 17:00:02 +0800

    updated5

--- api/test.ts
+++ api/test.ts
@@ -3,6 +3,6 @@ import type { VercelRequest, VercelResponse } from "@vercel/node";
 export default async (request: VercelRequest, response: VercelResponse) => {
   // console.log(request);
 
-  console.log(request.body);
+  console.log(request);
   response.status(200).send("Hello World!");
 };
